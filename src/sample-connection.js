2ebcc0c4f280b5ffc77fc0a62cab64bbed978d79
timeonator <timeonator@gmail.com>
2021-05-10 18:20:32 -0700

    start script update

--- src/sample-connection.js
+++
@@ -1,8 +0,0 @@
-var mysql = require('mysql')
-var connection = mysql.createConnection({
-  host: '<host>',
-  user: '<user>',
-  password: '<password>',
-  database: '<database>'
-});
-export default connection;
